722eec29f5d23dd12b2b00c8dbd0c74da53b467c
AbdulbositGo <abdulbosit.go@gmail.com>
2022-05-27 00:03:38 +0500

    Topic, Room, Messages models added

--- base/models.py
+++ base/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.body[:50]
+
+
--- base/views.py
+++ base/views.py
@@ -1,7 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {"id":1, "name": "python"},
+    {"id":2, "name": "c++"},
+    {"id":3, "name": "go"},
+]
+
 
 def home(request):
-    return
+    return HttpResponse()
